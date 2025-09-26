18e93c6d4956b1edc8200b9ea7dfa301ea181a62
MahMouD-YUsUF-M <mahmoudyusuf712@gmail.com>
2025-09-27 00:16:58 +0300

    implement tables

--- src/liburlshortner/data/models/tables.py
+++ src/liburlshortner/data/models/tables.py
@@ -1,5 +1,5 @@
 import sqlalchemy as sa
-from sqlalchemy import text, types
+from sqlalchemy import text, types, ForeignKey
 from sqlalchemy.dialects import mysql
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -31,5 +31,46 @@ INT = mysql.INTEGER(unsigned=True)
 BIGINT = mysql.BIGINT(unsigned=True)
 SINT = mysql.INTEGER(unsigned=False)
 SBIGINT = mysql.BIGINT(unsigned=False)
+
+
+class User(Model):
+    __tablename__ = 'user'
+
+    id_user = sa.Column(sa.Integer, primary_key=True)
+    user_code = sa.Column(sa.String(50), nullable=False,unique=True ,index=True)
+    is_guest = sa.Column(sa.Boolean, nullable=False, default=True)
+
+    created_at = sa.Column(types.TIMESTAMP,server_default=text('CURRENT_TIMESTAMP'),
+                           nullable=False, index=True)
+
+    updated_at = sa.Column(types.TIMESTAMP,
+                           server_default=text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+                           nullable=False, index=True)
+
+class Url(Model):
+    __tablename__ = 'url'
+    id_url = sa.Column(sa.Integer, primary_key=True)
+    url_code = sa.Column(sa.String(7), nullable=False,unique=True ,index=True)
+
+    target_url = sa.Column(sa.String(255), nullable=False,index=True)
+    id_user = sa.Column(sa.Integer,nullable=False,index=True)
+    expires_at = sa.Column(types.TIMESTAMP,nullable=False,index=True)
+
+    created_at = sa.Column(types.TIMESTAMP, server_default=text('CURRENT_TIMESTAMP'),
+                           nullable=False, index=True)
+
+    updated_at = sa.Column(types.TIMESTAMP,
+                           server_default=text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+                           nullable=False, index=True)
+
+class Click(Model):
+    __tablename__ = 'click'
+    id_click = sa.Column(sa.Integer, primary_key=True)
+    id_url = sa.Column(sa.Integer,nullable=False,index=True)
+    created_at = sa.Column(types.TIMESTAMP, server_default=text('CURRENT_TIMESTAMP'),
+                           nullable=False, index=True)
+    updated_at = sa.Column(types.TIMESTAMP,
+                           server_default=text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+                           nullable=False, index=True)
 
 
